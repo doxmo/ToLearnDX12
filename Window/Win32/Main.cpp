6b637744d67f8341a302c9a587522885cfd32689
Carlos Osnaya <doxmo@outlook.com>
2018-03-19 12:42:12 -0600

    Sin cambio sunstancial.

--- Window/Win32/Main.cpp
+++ Window/Win32/Main.cpp
@@ -2,7 +2,6 @@
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow)
 {
-	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
